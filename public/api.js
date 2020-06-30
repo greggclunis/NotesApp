d7096db26ae7b39bfc899862d78bf61d31df7053
Gregg <greggclunis@gmail.com>
2020-06-30 15:08:15 -0400

    clicking note now pulls up note for editing

--- public/api.js
+++ public/api.js
@@ -1,24 +1,24 @@
 //Display notes on load
 var loadContent = document.getElementById('notes_container');
 window.addEventListener('load', getRequest);
-
+var noteData;
 function getRequest(event) {
     event.preventDefault();
     fetch('/notes')
     .then((response) => response.json())
     .then(function(data) {
-
+        noteData = data;
            for (var i in data) {
                 let textExcerpt = data[i].noteText.slice(0, 100);
                document.getElementById("notes_container").innerHTML += 
                     
-                    '<div class="card single_note">' +
+                    '<a data-toggle="modal" data-target="#exampleModal"><div class="card single_note" onClick="grabId(this)" id="note_'+ i +'">' +
                     '<img src="https://i1.wp.com/tinyleaps.fm/wp-content/uploads/2020/05/uqyhjogyxyy-scaled.jpg" class="card-img-top" alt="...">' +
                     '<div class="card-body">' +
                     '<p class="card-title">' + 
                     data[i].noteTitle + 
                     '</p> <p class="card-text">'+
-                    textExcerpt + '<br /><br />View Note</div> </div>'
+                    textExcerpt + '<br /><br />View Note</div> </div></a>'
                     
             } 
             
@@ -88,22 +88,25 @@ function newNote(event, post) {
 
 
 //Edit note
-const viewNote = document.getElementsByClassName('single_note');
 
+function grabId(card) {
+    var openNote = document.getElementById(card.id);
+    var noteTitle = document.getElementById('newNoteTitle');
+    var noteText = document.getElementById('newNoteText');
+    var integer = /[0-9]/;
 
-for (var i in viewNote) {
-    console.log(viewNote);
+   for (var i in noteData) {
+        if (integer.test(openNote.id) == i) {
+            noteTitle.value = noteData[i-1].noteTitle;
+            noteText.value = noteData[i-1].noteText;
+            
+        }
+    }
+    
+
 }
 
-/*
-noteArray.forEach(function(viewNote) {
-    noteArray.addEventListener('click', openNote);
-});
 
-function openNote(event) {
-    console.log(this.id);
-}
-*/
 
 
 
