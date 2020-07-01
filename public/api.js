6c4f75902d2497da65d792e2d07e73a15029ba85
Gregg <greggclunis@gmail.com>
2020-06-30 22:36:39 -0400

    working on edit functionality

--- public/api.js
+++ public/api.js
@@ -12,13 +12,12 @@ function getRequest(event) {
                 let textExcerpt = data[i].noteText.slice(0, 100);
                document.getElementById("notes_container").innerHTML += 
                     
-                    '<a data-toggle="modal" data-target="#exampleModal"><div class="card single_note" onClick="grabId(this)" id="note_'+ i +'">' +
-                    '<img src="https://i1.wp.com/tinyleaps.fm/wp-content/uploads/2020/05/uqyhjogyxyy-scaled.jpg" class="card-img-top" alt="...">' +
+                    '<a data-toggle="modal" data-target="#editModal"><div class="card single_note" onClick="grabId(this)" id="note_'+ i +'">' +
                     '<div class="card-body">' +
-                    '<p class="card-title">' + 
+                    '<p class="card-title"><b>' + 
                     data[i].noteTitle + 
-                    '</p> <p class="card-text">'+
-                    textExcerpt + '<br /><br />View Note</div> </div></a>'
+                    '</b></p> <p class="card-text">'+
+                    textExcerpt + '</div> </div></a>'
                     
             } 
             
@@ -63,7 +62,6 @@ var createNote = document.getElementById('create_note');
 createNote.addEventListener('submit', newNote);
 
 function newNote(event, post) {
-    console.log("Hello from inside the function");
     var noteTitle = event.target.noteTitle.value;
     var noteText = event.target.noteText.value;
     
@@ -88,22 +86,52 @@ function newNote(event, post) {
 
 
 //Edit note
-
+var editNoteId;
 function grabId(card) {
-    var openNote = document.getElementById(card.id);
-    var noteTitle = document.getElementById('newNoteTitle');
-    var noteText = document.getElementById('newNoteText');
-    var integer = /[0-9]/;
+    
+//Get data from target note and print to modal screen 
+const openNote = document.getElementById(card.id);
+const noteTitle = document.getElementById('editNoteTitle');
+const noteText = document.getElementById('editNoteText');
+const integer = /[0-9]/;
 
-   for (var i in noteData) {
-        if (integer.test(openNote.id) == i) {
-            noteTitle.value = noteData[i-1].noteTitle;
-            noteText.value = noteData[i-1].noteText;
-            
+    for (var i in noteData) {
+       const x = integer.exec(openNote.id);
+       if (x == i) {
+            noteTitle.value = noteData[i].noteTitle;
+            noteText.value = noteData[i].noteText;
+           editNoteId = noteData[i].id;
         }
     }
-    
+     
+}
 
+const editPost = document.getElementById('edit_note');
+editPost.addEventListener('submit', submitEdit);
+    
+function submitEdit (event, patch) {
+    var noteTitle = event.target.noteTitle.value;
+    var noteText = event.target.noteText.value;
+    
+    post = {
+        noteTitle: noteTitle,
+        noteText: noteText
+    }
+    
+    const options = {
+        method: 'PATCH',
+        body: JSON.stringify(post),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }
+    
+    const URL = `/notes/${editNoteId}`;
+    
+    return fetch(URL, options)
+    .then(response => response.json())
+    .then(data => console.log('movie to update: ', data));
+        
 }
 
 
